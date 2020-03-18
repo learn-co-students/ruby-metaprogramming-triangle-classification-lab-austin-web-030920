a65d5d238120a2cc6ff3aebafd5be96b527123b3
github username <github email address>
2020-03-18 15:50:32 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a <= 0 || b <= 0 || c <=0
+      raise TriangleError
+    elsif a + b <=c || a + c <= b || b + c <= a
+      raise TriangleError
+    end
+
+    if a == b && a == c && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+  class TriangleError < StandardError
+    def message
+      'Illegal Triangle'
+    end
+  end
 end
