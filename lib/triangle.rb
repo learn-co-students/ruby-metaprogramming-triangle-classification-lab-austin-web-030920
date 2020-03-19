8af2f8f46c6744b6588598045cab2730a3a5b1e7
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-03-19 18:24:37 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+
+
 class Triangle
   # write code here
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if (@x + @y) <= @z || (@x + @z) <= @y || (@y + @z) <= @x
+      raise TriangleError
+    elsif @x == @y && @y == @z
+      :equilateral
+    elsif @x == @y || @x == @z || @y == @z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a triangle!"
+    end
+  end
+
 end
